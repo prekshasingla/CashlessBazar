505cc33cbec83bcc3f339a8c130c8a157c4af82a
prekshasingla <prekshasingla@gmail.com>
2018-10-18 09:46:33 +0530

    fb login

--- app/src/main/java/com/example/prekshasingla/cashlessbazar/LoginFragment.java
+++ app/src/main/java/com/example/prekshasingla/cashlessbazar/LoginFragment.java
@@ -10,6 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewManager;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -18,6 +26,11 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import java.util.Arrays;
+
+import static android.provider.ContactsContract.Intents.Insert.EMAIL;
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 
 public class LoginFragment extends Fragment implements View.OnClickListener {
 
@@ -25,7 +38,8 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     private int RC_SIGN_IN=100;
     private GoogleSignInOptions gso;
     private GoogleSignInClient mGoogleSignInClient;
-
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -36,11 +50,14 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestIdToken(getString(R.string.default_web_client_id))
+//                        .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(getActivity());
 //
 
     }
@@ -55,6 +72,35 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 
         signInButton.setOnClickListener(this);
 
+         callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) rootView.findViewById(R.id.login_button_fb);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        loginButton.setFragment(this);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Log.e("success","yes");
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.e("error","yes");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e("error","yes");
+
+                // App code
+            }
+        });
+
+
         return rootView;
     }
 
@@ -65,6 +111,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
             case R.id.google_sign_in_button:
                 signIn();
                 break;
+
             // ...
         }
     }
@@ -72,8 +119,13 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    //fb request code= 64206
+    //fb resultcode=-1
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
@@ -82,6 +134,11 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+        }
+        else if(requestCode== 64206){
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+//            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
         }
     }
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
