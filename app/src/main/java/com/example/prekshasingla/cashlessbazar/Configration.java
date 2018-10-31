138eace3d658ca63f33fccde8962268077c502df
prakhar agarwal <halfbiteducation@gmail.com>
2018-10-31 21:29:33 +0530

    changes

--- app/src/main/java/com/example/prekshasingla/cashlessbazar/Configration.java
+++ app/src/main/java/com/example/prekshasingla/cashlessbazar/Configration.java
@@ -3,4 +3,6 @@ package com.example.prekshasingla.cashlessbazar;
 public class Configration {
 
     final public static String ClientID="178902503529-f3h5aqh01ju6j6jvrnch4vm67j7lk1ld.apps.googleusercontent.com" ;
+
+    final public  static String urlPayFetchToken= "http://api2.cashlessbazar.com/api/getAccessToken";
 }
--- app/src/main/java/com/example/prekshasingla/cashlessbazar/MainActivity.java
+++ app/src/main/java/com/example/prekshasingla/cashlessbazar/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.prekshasingla.cashlessbazar;
 import android.content.Intent;
 import android.app.Activity;
 
-import instamojo.library.InstapayListener;
-import instamojo.library.InstamojoPay;
 
 import org.json.JSONObject;
 import org.json.JSONException;
@@ -30,45 +28,10 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
 
 
-    private void callInstamojoPay(String email, String phone, String amount, String purpose, String buyername) {
-        final Activity activity = this;
-        InstamojoPay instamojoPay = new InstamojoPay();
-        IntentFilter filter = new IntentFilter("ai.devsupport.instamojo");
-        registerReceiver(instamojoPay, filter);
-        JSONObject pay = new JSONObject();
-        try {
-            pay.put("email", email);
-            pay.put("phone", phone);
-            pay.put("purpose", purpose);
-            pay.put("amount", amount);
-            pay.put("name", buyername);
-            pay.put("send_sms", true);
-            pay.put("send_email", true);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        initListener();
-        instamojoPay.start(activity, pay, listener);
-    }
-
-    InstapayListener listener;
 
 
-    private void initListener() {
-        listener = new InstapayListener() {
-            @Override
-            public void onSuccess(String response) {
-                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG)
-                        .show();
-            }
 
-            @Override
-            public void onFailure(int code, String reason) {
-                Toast.makeText(getApplicationContext(), "Failed: " + reason, Toast.LENGTH_LONG)
-                        .show();
-            }
-        };
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -160,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
             case R.id.action_settings:
-                callInstamojoPay("xyz@gmail.com", "9958596472", "125", "abc", "prakhar");
                 return true;
         }
         return super.onOptionsItemSelected(item);
